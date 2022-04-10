08e1975b5e981ccd91b9cd4c8dd5af8c5df3f165
Thais Reis <thaisreisbueno@hotmail.com>
2022-04-09 21:11:55 -0300

    commet new

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Indicavi.Projeto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
