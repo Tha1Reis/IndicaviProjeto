6e6ec32e873b4b797b49ae75b699756fc24063c0
Thais Reis <thaisreisbueno@hotmail.com>
2022-04-26 16:55:33 -0300

    Upgrade

---
+++ wwwroot/js/email-senha.js
@@ -0,0 +1,47 @@
+﻿const buttonSignUp = document.querySelector('button[name=signup]');
+const buttonSignIn = document.querySelector('button[name=signin]');
+
+const inputEmail = document.querySelector('input[name=email');
+const inputPassword = document.querySelector('input[name=password');
+
+const firebaseAuth = firebase.auth();
+
+const handleAuthError = error => {
+
+    if(error.code){
+        switch(error.code){
+            case 'auth/weak-password':
+            alert('Senha fraca');
+            break;
+            case 'auth/argument-error':
+            alert('Incorreto');
+            break;
+            case 'auth/email-already-in-use':
+            alert('E-mail já em uso');
+            break;
+
+        }
+    }
+    console.log('handling');
+    console.log(error);
+}
+
+buttonSignUp.addEventListener('click', () => {
+    firebaseAuth.createUseWithEmailAndPassword(inputEmail.value, inputPassword.value).then(result => {
+            alert ('Conta criada com sucesso');
+            console.log(result);
+            const user = result.user;
+            user.sendEmailVerification().then( r => {
+                alert('Um e-mail de confirmação foi enviado');
+            }).catch(e => alert ('Houver um erro ao enviar o e-mail'));
+        }).catch(handleAuthError);
+        });
+
+
+buttonSignIn.addEventListener('click', () => {
+    firebaseAuth.signInWithEmailAndPassword(inputEmail.value, inputPassword.value);
+    then(result => {
+        console.log(result);
+    }).catch(handleAuthError);
+    })
+
---
+++ wwwroot/js/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClgBHeUhTk1pHGa8_3LMGbsy09Albl_uE",
+  authDomain: "indicavi.firebaseapp.com",
+  projectId: "indicavi",
+  storageBucket: "indicavi.appspot.com",
+  messagingSenderId: "660593850542",
+  appId: "1:660593850542:web:b63d06925be675ac9cd53f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
